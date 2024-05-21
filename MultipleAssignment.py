1a88d94ed5cdfb477cb572d3f996c8e84f6023ba
kenidervishaj <krenar.dervishaj@fau.de>
2024-05-21 22:40:04 +0200

    Initial Commit for the playground. Learning the fundamentals of Programming with Python. They come into use mostly for the syntax, and a good refresher too.

---
+++ MultipleAssignment.py
@@ -0,0 +1,23 @@
+# Simple assignment
+name = "Keni"
+age = 21
+attractive = True
+
+print(name)
+print(age)
+print(attractive)
+
+# Multiple
+
+name, age, attractive = "Bro", 21, True
+
+print(name)
+print(age)
+print(attractive)
+
+Spongebob = Patrick = Sandy = Squidward = 30
+
+print(Spongebob)
+print(Patrick)
+print(Sandy)
+print(Squidward)
---
+++ Variables.py
@@ -0,0 +1,21 @@
+# Strings
+first_name = 'John'
+last_name = 'Doe'
+full_name = first_name + ' ' + last_name
+
+print("Hello " + full_name)
+
+# Integers
+age = 21
+# age += 1
+print("Your age is: " + str(age))
+
+# Float (decimal)
+height = 250.5
+print("Your height is: " + str(height) + "cm")
+
+# Boolean
+human = True # or False
+print(human)
+print(type(human))
+print("Are you a human: " + str(human))
