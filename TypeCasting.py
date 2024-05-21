8ddf0a5581870719704d217a68406ef17d14d408
kenidervishaj <krenar.dervishaj@fau.de>
2024-05-21 22:59:25 +0200

    Type Casting methods

---
+++ TypeCasting.py
@@ -0,0 +1,18 @@
+# type casting = converting the data type of a value to another data type
+
+x = 1 # int
+y = 2.0 # float
+z = "3" # str
+
+x = float(x) # convert int to float
+y = int(y) # convert float to int permenantly
+z = float(z) # convert str to int
+
+print(x)
+print(int(y)) # convert float to int temporarily
+print(y)
+print(z)
+
+# Type casting is useful to print two different data types together
+
+
