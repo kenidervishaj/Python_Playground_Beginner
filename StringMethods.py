2f1efb0546566f5e5f01fa019f3a9cd3672b208e
kenidervishaj <krenar.dervishaj@fau.de>
2024-05-21 22:53:39 +0200

    A list of a few useful string methods

---
+++ StringMethods.py
@@ -0,0 +1,12 @@
+name = "Keni"
+
+print(len(name)) # length of the string
+print(name.find("e")) # find the index of the first occurrence of the substring, case sensitive
+print(name.capitalize()) # capitalize the first letter of the string
+print(name.upper()) # convert the string to uppercase
+print(name.lower()) # convert the string to lowercase
+print(name.isdigit()) # check if the string is a digit
+print(name.isalpha()) # check if the string is an alphabet
+print(name.count("e")) # count the number of occurrences of the substring
+print(name.replace("e", "a")) # replace the substring with another substring
+print(name*3) # repeat the string n times
