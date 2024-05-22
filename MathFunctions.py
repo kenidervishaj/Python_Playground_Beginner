199a1240ccb1e6fba3ab00849a88f665c7b5dffd
kenidervishaj <krenar.dervishaj@fau.de>
2024-05-22 22:37:31 +0200

    Math Functions

---
+++ MathFunctions.py
@@ -0,0 +1,20 @@
+import math
+
+pi = 3.14
+x = 1
+y = 2
+z = 3
+
+print(round(pi)) # round to the nearest whole number
+print(math.ceil(pi)) # round up
+print(math.floor(pi)) # round down
+print(abs(pi)) # absolute value
+print(pow(pi, 2)) # power of 2
+print(math.sqrt(420)) # square root
+print(math.exp(1)) # e^1
+print(math.log(math.exp(1))) # ln(e)
+print(math.sin(0)) # sin(0)
+print(math.cos(0)) # cos(0)
+print(math.tan(0)) # tan(0)
+print(max(x,y,z))
+print(min(x,y,z))
