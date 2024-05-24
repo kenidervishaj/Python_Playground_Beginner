3c289a3248e1fdda145b0d318620293cbfc5740d
kenidervishaj <krenar.dervishaj@fau.de>
2024-05-24 20:41:31 +0200

    Nested Function calls

---
+++ NestedFunctions.py
@@ -0,0 +1,6 @@
+# nested functions calls = function inside a function
+# nested functions are used to avoid code duplication
+# nested functions are used to avoid code redundancy
+# nested functions are used to avoid code complexity
+
+print(round(abs(float(input("Enter a whole positive number: ")))))
