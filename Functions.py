00b972d0ef04f99f099854c0c2de35228878a6d4
kenidervishaj <krenar.dervishaj@fau.de>
2024-05-24 17:29:19 +0200

    Functions in python and passing parameters to them

---
+++ Functions.py
@@ -0,0 +1,8 @@
+# function - a block of code which only runs when it is called
+
+def hello(first_name, last_name, age):
+    print("Hello " + first_name + " " + last_name + "!")
+    print("You are " + str(age) + " years old.")
+    print("Nice to meet you!")
+
+hello("Krenar", "Derivshaj", 21)
